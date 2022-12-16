2b19061b8156f9182197f8a8d425c06378efd2fc
07.Mayank-raj <2003.mayank.raj@gmail.com>
2022-12-16 23:22:24 +0530

    wishlist part

--- scripts/product.js
+++ scripts/product.js
@@ -18,7 +18,6 @@ async function fetchData() {
   } catch (error) {
     console.log(error);
   }
-
 }
 // onload -------------------->
 window.addEventListener("load", () => {
@@ -34,6 +33,7 @@ function getCard(data) {
                 <div class=" product-cards-fav">
                     <div>
                     <lord-icon
+                    class="fav-btn"
                     src="https://cdn.lordicon.com/pnhskdva.json"
                     trigger="hover"
                     style="width:40px;height:40px">
@@ -67,36 +67,91 @@ function getCard(data) {
 function renderData(data) {
   let product_container = document.querySelector(".product-container-reder");
   product_container.innerHTML = getCard(data);
+
+  // fetching all fav button after data displayed on DOM
+
+  let all_fav_btns = document.querySelectorAll(".fav-btn");
+  for (let btn of all_fav_btns) {
+    btn.addEventListener("click", (event) => {
+      let product_id = event.path[3].id;
+      console.log(product_id);
+      add_to_fav(product_id);
+    });
+  }
 }
 
+// -------------------------> add to favrite
+let add_to_fav = async (product_id) => {
+  console.log(product_id);
+  try {
+    let product = await getProduct(product_id);
+    // product = await JSON.stringify(product);
+    console.log(product);
+    
+    let add_to_fav_res = await fetch(`${baseURL}/wishlist`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+    // let  res = await (add_to_fav.json())
+    console.log(res);
+  } catch (error) {
+    alert("Trouble in adding product to cart");
+  }
+};
+
+// will get the product form server
+let getProduct = async (id) => {
+  try {
+    let product_res = await fetch(`${baseURL}/products/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "applicaton/json",
+      },
+    });
+    if (product_res.ok) {
+      let product = await product_res.json();
+      return product;
+    } else {
+      alert("alert in getting the data");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // --------------------------> sorting feature
-let sorting_btn = document.querySelector('#sort-by')
-sorting_btn.addEventListener('change', () => {
+let sorting_btn = document.querySelector("#sort-by");
+sorting_btn.addEventListener("change", () => {
   console.log(sorting_btn.value);
-  if (sorting_btn.value === 'default') fetchData();
+  if (sorting_btn.value === "default") fetchData();
 
-  if (sorting_btn.value === 'price-ases') {
-    let sort_url = 'https://639869fbfe03352a94d003fc.mockapi.io/products?sortBy=price&order=aesc&p=1&l=24'
-    fetchSortedData(sort_url);
-  };
-  if (sorting_btn.value === 'price-dses') {
-    let sort_url = 'https://639869fbfe03352a94d003fc.mockapi.io/products?sortBy=price&order=desc&p=1&l=24'
+  if (sorting_btn.value === "price-ases") {
+    let sort_url =
+      "https://639869fbfe03352a94d003fc.mockapi.io/products?sortBy=price&order=aesc&p=1&l=24";
     fetchSortedData(sort_url);
   }
-  if (sorting_btn.value === 'atoz') {
-    let sort_url = 'https://639869fbfe03352a94d003fc.mockapi.io/products?sortBy=title&order=aesc&p=1&l=24'
+  if (sorting_btn.value === "price-dses") {
+    let sort_url =
+      "https://639869fbfe03352a94d003fc.mockapi.io/products?sortBy=price&order=desc&p=1&l=24";
     fetchSortedData(sort_url);
   }
-  if (sorting_btn.value === 'ztoa') {
-    let sort_url = 'https://639869fbfe03352a94d003fc.mockapi.io/products?sortBy=title&order=desc&p=1&l=24'
+  if (sorting_btn.value === "atoz") {
+    let sort_url =
+      "https://639869fbfe03352a94d003fc.mockapi.io/products?sortBy=title&order=aesc&p=1&l=24";
     fetchSortedData(sort_url);
   }
-})
+  if (sorting_btn.value === "ztoa") {
+    let sort_url =
+      "https://639869fbfe03352a94d003fc.mockapi.io/products?sortBy=title&order=desc&p=1&l=24";
+    fetchSortedData(sort_url);
+  }
+});
 async function fetchSortedData(sort_url) {
   try {
     let response = await fetch(`${sort_url}`, {
-
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -112,5 +167,4 @@ async function fetchSortedData(sort_url) {
   } catch (error) {
     console.log(error);
   }
-
 }
