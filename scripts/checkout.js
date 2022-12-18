c8ac3f2805234c907d2342f13d4293642fd0fc9c
Hemandh7 <hemandh007bond@gmail.com>
2022-12-18 22:42:04 +0530

    day6

---
+++ scripts/checkout.js
@@ -0,0 +1,59 @@
+let baseURL = 'https://639869fbfe03352a94d003fc.mockapi.io'
+async function fetchData(){
+    try {
+        let response = await fetch(`${baseURL}/cart`,{
+            method : "GET",
+            headers : {
+                "Content-Type":"application/json"
+            }
+        })
+        if(response.ok){
+            let product_data = await response.json();
+            console.log(product_data);
+            
+            renderData2(product_data)
+        }else{
+            console.dir(response);
+        }
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+window.addEventListener("load",()=>{
+    fetchData();
+});
+
+
+function getItems2(data){
+    let cartData2=data.map((el,i)=>{
+        for(let i=1;i<=data.length;i++){
+        return `
+        <div id="giftcard">
+        <div>
+          <img src=${el.image} alt="">
+        </div>
+        <div>
+          <h4>${el.title}</h4>
+        </div>
+        <div>
+        <h5>${el.price}</h5>
+        </div>
+      </div>  
+        `;
+        };
+    });
+    return cartData2.join("");
+}
+
+
+function renderData2(data){
+    let summary = document.querySelector('#cart-append')
+    summary.innerHTML = getItems2(data);
+}
+
+let submit = document.querySelector('#submitorder');
+submit.addEventListener('click',()=>{
+    alert("Order Placed");
+    window.location.href="index.html"
+})
