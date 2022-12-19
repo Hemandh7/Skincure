97d8b2b2f97ef9f534c517bf5b1b05882fbdbf08
arundaspr <arundaspr1999@gmail.com>
2022-12-19 10:02:01 +0530

    submitting day6

--- scripts/register.js
+++ scripts/register.js
@@ -142,3 +142,14 @@ async function registeration(obj){
 
 
     }
+
+const navbar = document.querySelector('#NavBar');
+let top = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY >= top) {    
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');    
+  }
+}
+window.addEventListener('scroll', stickynavbar);
