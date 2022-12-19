c3c95fdcf2e4ec5d8356d7a7f8e17db078d678e9
07.Mayank-raj <2003.mayank.raj@gmail.com>
2022-12-19 15:32:26 +0530

    Login functionality done by : Arun

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,84 +1,64 @@
-var redirect = document.querySelector("#mybtn").addEventListener("click", registeration);
+var redirect = document
+  .querySelector("#mybtn")
+  .addEventListener("click", registeration);
 
-function registeration() 
-{
-    window.location.href = '../routes/register.html'
+function registeration() {
+  window.location.href = "../routes/register.html";
 }
 
-let baseURL =  `https://639869fbfe03352a94d003fc.mockapi.io`;
-
-
+let baseURL = `https://639869fbfe03352a94d003fc.mockapi.io`;
 
 document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    let obj={
-    'email' : document.querySelector("#email").value,
-    'password' : document.querySelector("#password").value
+  let obj = {
+    email: document.querySelector("#email").value,
+    password: document.querySelector("#password").value,
+  };
 
-    }
-    
-
-    // let valid = validForm(obj);
-
-    // if (valid == true) {
-    //     signin(obj);
-    // }
-    signin(obj);
-
+  signin(obj);
 });
 
-function validForm(obj) {
-    if (obj.email == "") {
-        document.getElementById("email").innerHTML = "Please fill the email";
-        return false;
+async function signin(obj) {
+  if (obj.email == "admin" && obj.password == "admin")
+    window.location.href = "../routes/admin.html";
+
+  console.log(obj);
+
+  try {
+    let login_request = await fetch(`${baseURL}/users/1`, {
+      method: "GET",
+      // body : JSON.stringify(obj),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    let userdata = await login_request.json();
+    if (userdata.email == obj.email && userdata.password == obj.password) {
+      alert("Logged-in ✅");
+      window.location.href = "../index.html";
+    } else if (userdata.email !== obj.email) {
+      alert("NO user found ❌");
+    } else {
+      alert("Wrong Password ❌");
     }
-
-    if (obj.password == "") {
-        document.getElementById("password").innerHTML = "Please fill the password";
-        return false;
-    }
-    if (obj.password.length <= 7 || obj.password.length > 12) {
-        document.getElementById("password").innerHTML = "Password should be in between 7 and 12";
-        return false;
-    }
-    return true;
-}
-
-
-
-async function signin(obj){
-
-    try {
-        let login_request = await fetch(`${baseURL}/users`,{
-            method : "GET",
-            // body : JSON.stringify(obj),
-            headers : {
-                "Content-Type" : "application/json"
-            },
-        })
-
-        let userdata = await login_request.json();
-        console.log(userdata);
-        console.log(obj);
-        if(login_request.ok){
-            window.location.href = "../index.html";
-        }else{
-            alert("User not found.");
-        }
-    } catch (error) {
-		console.log(error);
-        alert("wrong username or password. Please try again later.");
-    }
-}
-
-const navbar = document.querySelector('#NavBar');
-let top = navbar.offsetTop;
-function stickynavbar() {
-  if (window.scrollY >= top) {    
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');    
+    console.log(obj);
+  } catch (error) {
+    console.log(error);
+    alert("wrong username or password. Please try again later.");
   }
 }
-window.addEventListener('scroll', stickynavbar);
+
+const navbar = document.querySelector("#NavBar");
+let top = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY >= top) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+window.addEventListener("scroll", stickynavbar);
+
+Collapse;
